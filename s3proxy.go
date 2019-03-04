b5ff0917ea17f9383fdb8310cbcfff0e9c516125
Linus Yong <linusyong@gmail.com>
2019-03-04 19:40:52 +1100

    Added code from getting credentials from IAM Role Creds.

--- s3proxy.go
+++ s3proxy.go
@@ -4,18 +4,26 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// const S3Endpoint = "http://localhost:4572"
+const S3Endpoint = "http://localhost:4572"
 
 func S3Put(localfile string) {
 	s := session.Must(session.NewSession(&aws.Config{
 		S3ForcePathStyle: aws.Bool(true),
 		Region:           aws.String(endpoints.ApSoutheast2RegionID),
-		// Endpoint:         aws.String(S3Endpoint),
+		Credentials: credentials.NewCredentials(
+			&ec2rolecreds.EC2RoleProvider{
+				Client: ec2metadata.New(session.New()),
+			},
+		),
+		Endpoint: aws.String(S3Endpoint),
 	}))
 
 	c := s3.New(s, &aws.Config{})
